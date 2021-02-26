2cdd7c5b0b219097d9567411192e2cc00894ff2e
Philipp Möller <42586947+philipp-moeller@users.noreply.github.com>
2021-02-26 16:58:03 +0100

    Beispiel zum Nacharbeiten

--- pages/philip.js
+++ pages/philip.js
@@ -12,11 +12,14 @@ module.exports = async function(/* Hier können Daten eingelesen werden. */) {
 	// Beispiel:
 	// var user = new User(1);
 	// await user.init();
+	
+	var x = 12+19;
+	var y = "sehr" + "gut";
 
 	var html = new Template('./html/philip.html',{
 		// Hier können Daten an das HTML-Dokument übergeben werden.
-		note: "sehr gut",
-		punkte: 17
+		note: y,
+		punkte: x
 	});
 	return await html.parse();
 }
