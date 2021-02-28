4ca02dce4eefb1b4393dd60cdac0916fefa7a9ea
Tauschrausch-Team <cooljolol@gmx.de>
2021-02-28 13:50:53 +0100

    added db-table & inspect object

--- pages/articles.js
+++ pages/articles.js
@@ -1,19 +1,37 @@
 const Template = require('../template.js');
+const User = require('../db/user.js');
+const util = require('util');
+
 
 // Hier können Module geladen werden.
 //
 // Beispiel:
 // const User = require('../db/user.js');
 
-module.exports = async function(/* Hier können Daten eingelesen werden. */) {
+module.exports = async function() {
 
-	// read db-table and return it to articles.html -> table
-	// Beispiel:
-	// var user = new User(1);
-	// await user.init();
+	// editTable
+    var user = new User(1);  // erzeugt Nutzer mit `id=1`
+
+    await user.init();   // lädt die Daten aus Datenbank
+
+    user.name = "Josch";  // setzt eine Eigenschaft des Nutzers
+    await user.save();   // speichert den Nutzer in Datenbank
+
+    await user.init();   // lädt die Daten aus Datenbank
+	
+	console.log("Users-Table: ");
+	var tableData = util.inspect(user, {showHidden: false, depth: null});
+	console.log(tableData);
+
+	var userID = user.id;
+	var userName = user.name;
 
 	var html = new Template('./html/articles.html',{
         // pass through db-table/rows to html
+		userID: userID,
+		userName: userName,
+		tableData: user
 	});
 	return await html.parse();
 }
