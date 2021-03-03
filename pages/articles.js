0b6eef101c95afd32039d9ad832448742cedc8d5
Josch <cooljolol@gmx.de>
2021-03-03 19:10:51 +0100

    clean script

--- pages/articles.js
+++ pages/articles.js
@@ -3,53 +3,43 @@ const User = require('../db/user.js');
 const util = require('util');
 const db = require('../config.js');
 
-
-// Hier können Module geladen werden.
-//
-// Beispiel:
-// const User = require('../db/user.js');
-
 module.exports = async function() {
 
-	// editTable
-    var user = new User();  // erzeugt Nutzer mit `id=1`
+	// // editTable
+    // var user = new User();  // erzeugt Nutzer mit `id=1`
 
-    await user.init();   // lädt die Daten aus Datenbank
+    // await user.init();   // lädt die Daten aus Datenbank
 
-    user.name = "Josch";  // setzt eine Eigenschaft des Nutzers
-    await user.save();   // speichert den Nutzer in Datenbank
+    // user.name = "Josch";  // setzt eine Eigenschaft des Nutzers
+    // await user.save();   // speichert den Nutzer in Datenbank
 
-    await user.init();   // lädt die Daten aus Datenbank
+    // await user.init();   // lädt die Daten aus Datenbank
 
-	// does not work yet
-	for (var i = userID; i > 0; i--){
-		await user.init(i);
-		console.log("User:");
-		console.log(user.id);
-	}
+	// console.log("Users-Table: ");
+	// var tableData = util.inspect(user, {showHidden: false, depth: null});
+	// console.log(tableData);
 
-	console.log("Users-Table: ");
-	var tableData = util.inspect(user, {showHidden: false, depth: null});
-	console.log(tableData);
+	var userID; // = user.id;
+	var userName; // = user.name;
 
-	var userID = user.id;
-	var userName = user.name;
+	let tableContent = [];
 
 	// get user from db table
 	let dbTable = db.query("SELECT * FROM users");
 	dbTable.then(function(result) {
-		console.log(result);	// return whole array
 		// return every content alone
 		for(var i=result.length -1; i > -1; i--){
 			console.log(result[i]);
 		}
 	})
 
+	console.log(tableContent);
+
 	var html = new Template('./html/articles.html',{
         // pass through db-table/rows to html
 		userID: userID,
 		userName: userName,
-		tableData: user
+		tableData: 0
 	});
 	return await html.parse();
 }
