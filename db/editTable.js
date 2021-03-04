d3429e976e2b1f33e6caf0be31171cb6a2ca02ce
Josch <cooljolol@gmx.de>
2021-03-04 17:38:34 +0100

    add user exist ind table method

--- db/editTable.js
+++ db/editTable.js
@@ -1,19 +1,42 @@
 // http://localhost/phpmyadmin/ -> view db from xampp
-const mysql = require('mysql');
 const User = require('../db/user.js');
+const db = require('../config.js');
 
-module.export = async function (id, username){
-    // editTable
-    var user = new User(id);  // erzeugt Nutzer mit `id=1`
 
-    user.init();   // lädt die Daten aus Datenbank
-    console.log(user.name);  // gibt den Namen des Nutzers aus
 
-    user.name = username;  // setzt eine Eigenschaft des Nutzers
-    user.save();   // speichert den Nutzer in Datenbank
+const existUser = (userName, userPassword) => {
+    // get user from db table
+	let tableContent = await db.query("SELECT * FROM users");
+	console.table(tableContent[1].id); // [index] from object table
 
-    user.init();   // lädt die Daten aus Datenbank
-    console.log(user.name);  // gibt den Namen des Nutzers aus
+    // compare each data of tableContent objects
+    tableContent.array.forEach(element => {
+        if ((element.name.equals(userName))) {
+            console.log("YEEEEE!!")
+            break;
+        }
+    });
 }
+
+const otherMethod = () => {
+
+}
+
+modules.exports = {
+    existUser,
+    otherMethod
+};
+
+    // // editTable
+    // var user = new User(id);  // erzeugt Nutzer mit `id=1`
+
+    // user.init();   // lädt die Daten aus Datenbank
+    // console.log(user.name);  // gibt den Namen des Nutzers aus
+
+    // user.name = username;  // setzt eine Eigenschaft des Nutzers
+    // user.save();   // speichert den Nutzer in Datenbank
+
+    // user.init();   // lädt die Daten aus Datenbank
+    // console.log(user.name);  // gibt den Namen des Nutzers aus
   
 // module.exports = makeStandartUser;
