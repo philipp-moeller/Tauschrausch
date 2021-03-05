b0139673b4457e3a50a6687b55f8840d5f096825
Christian <tausch-rausch@mein.gmx>
2021-03-05 18:18:53 +0100

    modified christian.js

--- pages/christian.js
+++ pages/christian.js
@@ -5,7 +5,7 @@ const Template = require('../template.js');
 // Beispiel:
 // const User = require('../db/user.js');
 
-module.exports = async function(iqpath) {
+module.exports = async function(path1) {
 
 	// Hier können Daten aus der Datenbank gelesen und / oder verarbeitet werden.
 	//
@@ -14,13 +14,13 @@ module.exports = async function(iqpath) {
 	// await user.init();
 
 	var iqwert = Math.floor(Math.random() * (90 - 50 + 1) + 50);
-	var iqresultcombine = String(iqwert) + String(iqpath);
-	var iqresultadd = Number(iqwert) + Number(iqpath);
+	var iqresultcombine = String(iqwert) + String(path1);
+	var iqresultadd = Number(iqwert) + Number(path1);
 
 	var html = new Template('./html/christian.html',{
 		// Hier können Daten an das HTML-Dokument übergeben werden.
 		iq1: iqwert,
-		iq2: iqpath,
+		iq2: path1,
 		iqresultcomb: iqresultcombine,
 		iqresult: iqresultadd
 	});
