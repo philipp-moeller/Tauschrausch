e0bd769642a83fa049627e8b09173fdc5ecb456c
Josch <cooljolol@gmx.de>
2021-03-11 12:34:23 +0100

    create user in login - in progress..

--- pages/login.js
+++ pages/login.js
@@ -2,10 +2,26 @@ const Template = require('../template.js');
 const User = require('../db/user.js');
 
 // add a popup window, when creating a new acc. (action by click button)
-module.exports = async function() {
+module.exports = async function(query) {
 
+	// create new User
+    var user = new User();  // erzeugt Nutzer mit `id=0`
+
+    await user.init();   // lädt die Daten aus Datenbank
+    console.log("Username before save:" + user.name);  // gibt den Namen des Nutzers aus
+
+    user.name = "username";  // setzt eine Eigenschaft des Nutzers
+    await user.save();   // speichert den Nutzer in Datenbank
+
+    await user.init();   // lädt die Daten aus Datenbank
+    console.log("Username after save:" + user.name);  // gibt den Namen des Nutzers aus
+
+	console.log("query data:")
+	console.log(query);
+
+	// templating in html
 	var html = new Template('./html/login.html',{
-		// Hier können Daten an das HTML-Dokument übergeben werden.
+		
 	});
 	return await html.parse();
 }
--- pages/menu.js
+++ pages/menu.js
@@ -1,23 +1,7 @@
 const Template = require('../template.js');
 const User = require('../db/user.js');
 
-module.exports = async function(username, password, query) {
-
-	// editTable
-    var user = new User(0);  // erzeugt Nutzer mit `id=0`
-
-    await user.init(0);   // lädt die Daten aus Datenbank
-    console.log("Username before save:" + user.name);  // gibt den Namen des Nutzers aus
-
-    user.name = username;  // setzt eine Eigenschaft des Nutzers
-    await user.save();   // speichert den Nutzer in Datenbank
-
-    await user.init();   // lädt die Daten aus Datenbank
-    console.log("Username after save:" + user.name);  // gibt den Namen des Nutzers aus
-
-	// add username to html
-	console.log("query data:")
-	console.log(query);
+module.exports = async function(username) {
 
 	var html = new Template('./html/menu.html',{
 		username: username
