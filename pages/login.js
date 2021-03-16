68bb9bafbb4eacf8085e5d585f3d3804ec810909
Josch <cooljolol@gmx.de>
2021-03-16 08:24:38 +0100

    solved popup-window error

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ const Template = require('../template.js');
 const User = require('../db/user.js');
 
 // add a popup window, when creating a new acc. (action by click button)
-module.exports = async function(query) {
+module.exports = async function(query, username) {
 
 	// create new User
     var user = new User();  // erzeugt Nutzer mit `id=0`
@@ -10,7 +10,7 @@ module.exports = async function(query) {
     await user.init();   // lädt die Daten aus Datenbank
     console.log("Username before save:" + user.name);  // gibt den Namen des Nutzers aus
 
-    user.name = "username";  // setzt eine Eigenschaft des Nutzers
+    user.name = username;  // setzt eine Eigenschaft des Nutzers
     await user.save();   // speichert den Nutzer in Datenbank
 
     await user.init();   // lädt die Daten aus Datenbank
--- script/util.js
+++ script/util.js
@@ -1,7 +1,4 @@
-
-// Uncaught ReferenceError: button is not defined
-// at util.js:2
-document.getElementById(button).addEventListener("click", function(){
+document.getElementById("popupButton").addEventListener("click", function(){
 	// make popup window visible
 	document.querySelector(".popup").style.display = "flex";
 	console.log("pressed on popup-button");
