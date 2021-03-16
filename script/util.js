aacdb908a93e28247d209765574e618720c32f7f
Josch <cooljolol@gmx.de>
2021-03-16 19:00:52 +0100

    add empty username-field-alert

--- script/util.js
+++ script/util.js
@@ -1,3 +1,5 @@
+
+// open/close popup window
 document.getElementById("popupButton").addEventListener("click", function(){
 	// make popup window visible
 	document.querySelector(".popup").style.display = "flex";
@@ -7,3 +9,12 @@ document.getElementById("popupButton").addEventListener("click", function(){
 document.querySelector(".close").addEventListener("click", function(){
 	document.querySelector(".popup").style.display = "none";
 });
+
+// check if login-textfield is empty
+function validateForm() {
+	var x = document.forms["loginForm"]["username"].value;
+	if (x == "") {
+	  alert("Name must be filled out");
+	  return false;
+	}
+}  
