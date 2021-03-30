4d547a338f2102f2e73b2627fca4764c476df7b9
Josch <cooljolol@gmx.de>
2021-03-30 16:50:18 +0200

    easter egg (title icon)

--- script/util.js
+++ script/util.js
@@ -4,11 +4,10 @@ function animate(options) {
 	var start = performance.now();
   
 	requestAnimationFrame(function animate(time) {
-	  // timeFraction от 0 до 1
+	  // timeFraction
 	  var timeFraction = (time - start) / options.duration;
 	  if (timeFraction > 1) timeFraction = 1;
   
-	  // текущее состояние анимации
 	  var progress = options.timing(timeFraction)
 	  
 	  options.draw(progress);
